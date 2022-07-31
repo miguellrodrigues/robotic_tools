7fb8d2ae8277dd16a4bcec52e9b7de8385fe1622
Miguel <miguellukas52@gmail.com>
2022-07-31 18:42:12 -0300

    dynamics--

--- lib/forward_dynamics/__init__.py
+++ lib/forward_dynamics/__init__.py
@@ -9,33 +9,48 @@ class ForwardDynamics:
 
     self.q = sp.Matrix([link.generalized_coordinate for link in self.links])
     self.dq_dt = self.q.diff(t)
+    self.d2q_dt = self.dq_dt.diff(t)
 
     self.len_q = len(self.q)
 
     self.w = self.jacobian[3:, :]
-
-    k, p = self.get_total_energy()
-
-    self.total_lagrangian = k - p
     self.equations = self.get_system_equations_of_motion()
 
   def get_system_equations_of_motion(self):
     equations = []
+    D, P = self.get_inertia_matrix_and_potential_energy()
 
-    for i in range(len(self.links)):
-      q = self.q[i]
-      dq_dt = self.dq_dt[i]
-      tau = sp.Symbol(f'tau_{i + 1}')
+    for k in range(len(self.links)):
+      qk = self.q[k]
 
-      EQ = sp.diff(sp.diff(self.total_lagrangian, dq_dt), t) - sp.diff(self.total_lagrangian, q)
+      tau = sp.Symbol(f'tau_{k + 1}')
+      gk = P.diff(qk)
+
+      sum_a = 0
+      sum_b = 0
+
+      for i in range(self.len_q):
+        qi = self.q[i]
+
+        for j in range(self.len_q):
+          qj = self.q[j]
+          sum_a += D[k, j] * sp.diff(qj, t, 2)
+
+          dkj = D[k, j]
+          dij = D[i, j]
+
+          aux = (dkj.diff(qi) - sp.Rational(1, 2) * dij.diff(qk)) * self.dq_dt[i] * self.dq_dt[j]
+          sum_b += aux
+
+      tau_k = sum_a + sum_b + gk
 
       equations.append(
-        sp.Eq(tau, sp.simplify(EQ[0]))
+        sp.Eq(tau, sp.simplify(tau_k))
       )
-
+    
     return equations
 
-  def get_total_energy(self):
+  def get_inertia_matrix_and_potential_energy(self):
     # translational_kinetic_energy = 0
     potential_energy = sp.zeros(1, 1)
     D = sp.zeros(self.len_q, self.len_q)
@@ -59,7 +74,5 @@ class ForwardDynamics:
       D += (m * Jvi.T @ Jvi) + (Jwi.T @ I @ Jwi)
       potential_energy += m * G.T @ r
 
-    K = sp.Rational(1, 2) * self.dq_dt.T @ D @ self.dq_dt
-
-    return K, potential_energy
+    return D, potential_energy[0]
 
