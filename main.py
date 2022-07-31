906d616ef1489002a393210a316f835fe5bb90d1
Miguel <miguellukas52@gmail.com>
2022-07-31 18:49:19 -0300

    dynamics--

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from lib.forward_kinematics import ForwardKinematic
 from lib.link import Link
 from lib.symbols import t
 
-a1, a2, a3 = sp.symbols('a_1 a_2 a_3')
+a1, a2 = sp.symbols('a_1 a_2')
 
 q1 = sp.Function('q_1')(t)
 q2 = sp.Function('q_2')(t)
-q3 = sp.Function('q_3')(t)
 
-j0 = Link([q1, 0, a1, 0])
+j0 = Link([q1, 0, a1, sp.pi/2])
 j1 = Link([q2, 0, a2, 0])
-j2 = Link([q3, 0, a3, 0])
 
 fk = ForwardKinematic([j0, j1])
 
